3de4e8b23ff8aefd5eb8f5cf0c99de2d1cbb3fa3
canonall <onalcan20@gmail.com>
2021-11-28 16:09:30 +0100

    DeepLink widget and notification added

--- app/src/main/java/com/canonal/jetpacknavigation/fragment/DeepLinkFragment.kt
+++ app/src/main/java/com/canonal/jetpacknavigation/fragment/DeepLinkFragment.kt
@@ -1,10 +1,17 @@
 package com.canonal.jetpacknavigation.fragment
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.NotificationCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.canonal.jetpacknavigation.R
 import com.canonal.jetpacknavigation.databinding.FragmentDeepLinkBinding
 
 
@@ -26,6 +33,41 @@ class DeepLinkFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentDeepLinkBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        binding.tvText.text = arguments?.getString("myKey")
+        binding.btnSendNotification.setOnClickListener {
+            val bundle = Bundle()
+            bundle.putString("myKey", binding.etText.text.toString())
+
+            val deepLinkFromNotification = findNavController().createDeepLink()
+                .setDestination(R.id.deepLinkFragment)
+                .setArguments(bundle)
+                .createPendingIntent()
+
+            val notificationManager =
+                context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notificationManager.createNotificationChannel(
+                    NotificationChannel(
+                        "deepLink",
+                        "Deep Links",
+                        NotificationManager.IMPORTANCE_HIGH
+                    )
+                )
+            }
+            val builder = NotificationCompat.Builder(
+                requireContext(), "deepLink"
+            )
+                .setContentTitle("Notification Title")
+                .setContentText(binding.etText.text)
+                .setSmallIcon(R.drawable.ic_android)
+                .setContentIntent(deepLinkFromNotification)
+                .setAutoCancel(true)
+            notificationManager.notify(0, builder.build())
+        }
     }
 
     companion object {
--- app/src/main/java/com/canonal/jetpacknavigation/provider/DeepLinkWidgetProvider.kt
+++ app/src/main/java/com/canonal/jetpacknavigation/provider/DeepLinkWidgetProvider.kt
@@ -1,4 +1,35 @@
 package com.canonal.jetpacknavigation.provider
 
-class DeepLinkWidgetProvider {
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.os.Bundle
+import android.widget.RemoteViews
+import androidx.navigation.NavDeepLinkBuilder
+import com.canonal.jetpacknavigation.R
+
+class DeepLinkAppWidgetProvider : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        val remoteViews = RemoteViews(
+            context.packageName,
+            R.layout.widget_deep_link
+        )
+
+        //get this data inside DeepLinkFragment with arguments?.getString("myKey")
+        val args = Bundle()
+        args.putString("myKey", "From Widget")
+
+        val pendingIntent = NavDeepLinkBuilder(context)
+            .setGraph(R.navigation.nav_graph)
+            .setDestination(R.id.deepLinkFragment)
+            .setArguments(args)
+            .createPendingIntent()
+
+        remoteViews.setOnClickPendingIntent(R.id.btnWidgetDeepLink, pendingIntent)
+        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews)
+    }
 }
